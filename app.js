b9a789a12d30c25490afcd2779a02c6665198696
Aya <67323302+aya-abdelaal@users.noreply.github.com>
2022-12-28 23:46:07 +0200

    finish project

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(session({
 }));
 
 let msg = null;
+let city = null;
 
 
  //get requests
@@ -63,53 +64,74 @@ let msg = null;
   });
 
   app.get("/wanttogo", function (req, res) {
-    if(req.session.username)
-    res.render("wanttogo");
+    if(req.session.username){
+      userCollection.find({username: req.session.username}).toArray((err, doc) => {
+        res.render('wanttogo', {places:doc[0].wantToGo});
+      })
+    }
     else 
     res.redirect('/');
   });
 
   app.get("/bali", function (req, res) {
-    if(req.session.username)
-    res.render("bali", {video : "https://www.youtube.com/embed/LCqK7wZd2Pk"});
+    if(req.session.username){
+    res.render("bali", {video : "https://www.youtube.com/embed/LCqK7wZd2Pk", msg: msg});
+    city = "bali";
+    msg = null;
+    }
     else 
     res.redirect('/');
 
   });
 
   app.get("/inca", function (req, res) {
-    if(req.session.username)
-    res.render("inca", {video :  "https://www.youtube.com/embed/cnMa-Sm9H4k"});
+    if(req.session.username){
+    res.render("inca", {video :  "https://www.youtube.com/embed/cnMa-Sm9H4k", msg: msg});
+    msg = null;
+    city = "inca";
+    }
     else 
     res.redirect('/');
   });
 
   app.get("/annapurna", function (req, res) {
    
-    if(req.session.username)
-    res.render("annapurna", {video : "https://www.youtube.com/embed/nIzcnoyhzSQ"});
+    if(req.session.username){
+    res.render("annapurna", {video : "https://www.youtube.com/embed/nIzcnoyhzSQ", msg : msg});
+    msg = null;
+    city = "annapurna";
+    }
     else 
     res.redirect('/');
   });
 
   app.get("/paris", function (req, res) {
-    if(req.session.username)
-    res.render("paris", {video : "https://www.youtube.com/embed/UfEiKK-iX70"});
+    if(req.session.username){
+    res.render("paris", {video : "https://www.youtube.com/embed/UfEiKK-iX70", msg: msg});
+    msg = null;
+    city = "paris";
+    }
     else 
     res.redirect('/');
     
   });
 
   app.get("/rome", function (req, res) {
-    if(req.session.username)
-    res.render("rome", {video : "https://www.youtube.com/embed/oSexfR0Ubzw"});
+    if(req.session.username){
+    res.render("rome", {video : "https://www.youtube.com/embed/oSexfR0Ubzw", msg:msg});
+    msg = null;
+    city = "rome";
+    }
     else 
     res.redirect('/');
   });
 
   app.get("/santorini", function (req, res) {
-    if(req.session.username)
-    res.render("santorini", {video:"https://www.youtube.com/embed/cKedc8trR2Y"});
+    if(req.session.username){
+    res.render("santorini", {video:"https://www.youtube.com/embed/cKedc8trR2Y", msg:msg});
+    msg = null;
+    city = "santorini";
+    }
     else 
     res.redirect('/');
     
@@ -203,12 +225,62 @@ app.post('/', (req, res) => {
 }
 
 
+//adding to wantogo
+app.post('/addWantToGo', (req,res) => {
+  userCollection.find({username : username}).toArray((err,users) => {
+    if(users[0].wantToGo.includes(city))
+    msg = "city already added";
+    else {
+      userCollection.updateOne({username : req.session.username}, {$addToSet : {"wantToGo" : city}});
+  msg = "successfully added";
+    }
+    res.redirect("/" + city);
+  })  
+  console.log(city);
+  
+})
+
+
+
+//search
+function getAllSubstrings(s){
+  let i,j,res = [];
+
+  for(i = 0; i < s.length; i++){
+    for(j = i+1; j<s.length+1; j++){
+      res.push(s.slice(i,j));
+    }
+  }
+
+  return res;
+}
+
+let subs = [{city:"bali", subs: getAllSubstrings("bali")},
+{city:"rome", subs: getAllSubstrings("rome")},
+{city:"annapurna", subs: getAllSubstrings("annapurna")},
+{city:"inca", subs: getAllSubstrings("inca")},
+{city:"paris", subs: getAllSubstrings("paris")},
+{city:"santorini", subs: getAllSubstrings("santorini")}];
+
+
+
+app.post('/search', async (req,res) => {
+const x = req.body.Search;
+let output = [];
+
+subs.forEach(item =>  {
+  if(item.subs.includes(x.toLowerCase()))
+    output.push({title: item.city});
+})
+
+res.render('searchresults',{cities: output});
+});
+
+
+
+
 app.listen(3000);
 
-//added get requests le kolo to access pages
-//added post requests for usernames and passwords
-//need to embed vids
-//do kol haga database
-// and addd action buttons
+
 
 
